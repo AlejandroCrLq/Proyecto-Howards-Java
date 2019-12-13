3f08344eb8cee392115dadc3042c95dad8331a8a
oscarbm27 <obrenesmancera.sanjose@alumnado.fundacionloyola.net>
2019-12-13 12:14:28 +0100

    Subida la mayoría de los Listener de FTP.

---
+++ src/Events/ListenerAbout.java
@@ -0,0 +1,14 @@
+package Events;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ListenerAbout implements ActionListener{
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("pipo");
+	}
+	
+}
---
+++ src/Events/ListenerNameChange.java
@@ -0,0 +1,37 @@
+package Events;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import ftp.FTPController;
+
+public class ListenerNameChange implements ActionListener{
+	JLabel path;
+	JTextField fileName;
+	FTPController FtpController;
+	public ListenerNameChange(JLabel path,JTextField fileName,FTPController FtpController) {
+		this.path = path;
+		this.fileName = fileName;
+		this.FtpController = FtpController;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		File fileToChange = new File(path.getText());
+		File newName = new File(fileName.getText());
+		if (!newName.equals("")) {
+			if (fileToChange.renameTo(newName)) {
+				path.setText(newName.getName());
+				//Hay que hacer registro en la BD de el renombrado del archivo.
+			} else {
+				JOptionPane.showMessageDialog(FtpController.getFtpWindow(), "Error al renombrar archivo.", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}else {
+			JOptionPane.showMessageDialog(FtpController.getFtpWindow(), "La caja de texto no puede estar vacía.", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+}
