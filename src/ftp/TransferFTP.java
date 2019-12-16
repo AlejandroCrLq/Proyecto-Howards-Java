e2a5a6a53365ce4d813a6038c55544d7ea5d4410
RBarrancoAntunez <rbarrancoantunez.sanjose@alumnado.fundacionloyola.net>
2019-12-16 09:04:25 +0100

    Recuperación de los archivos de FTP con descarga incluida

--- src/ftp/TransferFTP.java
+++ src/ftp/TransferFTP.java
@@ -1,47 +1,71 @@
 package ftp;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.io.OutputStream;
 
 import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 public class TransferFTP {
 
 	private Users user;
 
-	public TransferFTP(Users user) {
-		this.user = user;
+	public static void main(String[] args) {
+
+		/*
+		 * String password = "1234567890"; 
+		 * MessageDigest digest; 
+		 * byte[] hash = null; 
+		 * try
+		 * { 
+		 * 		digest = MessageDigest.getInstance("SHA-256"); 
+		 * 		hash = digest.digest(password.getBytes(StandardCharsets.UTF_8)); 
+		 * } catch (NoSuchAlgorithmException e) {
+		 * System.out.println("Error en la encriptación de la contraseña.");
+		 * }
+		 */
+
+		Users user = new Users();
+		user.seteMail("rbarranco");
+		user.setUserName("Rafael Barranco");
+		user.setPassword("1234567890");
+		user.setTeacher(false);
 
 		String fileName = "gollum.jpg";
 		String directory = "C:\\Users\\rafae\\OneDrive\\Documentos\\Proyecto Multidisciplinar\\";
 		String remoteFolder = "..";
+
+		// File fileFromPicker = new File(directory, fileName);
+		String fileFromPicker = "peval1";
+		String local = "C:\\peval3\\";
 		
-		//File fileFromPicker = new File(directory, fileName);
-		File fileFromPicker = new File("C:\\peval1");
+		//TransferFTP t = new TransferFTP(user, fileFromPicker, remoteFolder);
+		TransferFTP t = new TransferFTP(user, local, fileFromPicker);
+		
 
-		FTPClient client = new FTPClient();
+	}
 
-		String sFTP = "localhost";
-		int port = 21;
-		String sUser = user.geteMail();
-		String sPassword = user.getPassword();
+	public TransferFTP(Users user, String local, String remote) {
+		this.user = user;
 
 		try {
-			client.connect(sFTP, port);
+			ClientFTP client = new ClientFTP(this.user);
+
+			client.connect();
 			if (client.isConnected()) {
-				boolean login = client.login(sUser, sPassword);
+				boolean login = client.login();
 				if (login) {
 					client.enterLocalPassiveMode();
 					client.setFileType(FTP.BINARY_FILE_TYPE);
+					client.setControlEncoding("UTF-8");
 
-					loadFiles(client, fileFromPicker, remoteFolder);
+					downloadFiles(client, local, remote);
 
 					client.logout();
 					client.disconnect();
@@ -52,26 +76,66 @@ public class TransferFTP {
 				System.out.println("No ha conectado.");
 			}
 
-		} catch (IOException ioe) {
-			System.out.println("Error de E/S en la conexión. " + ioe.getMessage());
+		} catch (IOException ex) {
+			System.out.println("Error de E/S en la conexión. " + ex.getMessage());
 		}
 	}
 
-	public void loadFiles(FTPClient client, File file, String remoteFolder) throws IOException {
+	public void loadFiles(ClientFTP client, String localFile, String remoteFile) throws IOException {
+		File file = new File(localFile);
 		if (file.exists()) {
 			if (file.isDirectory()) {
 				File[] files = file.listFiles();
-				String newRemoteFolder = remoteFolder + "\\" + file.getName();
+				String newRemoteFolder = remoteFile + "\\" + file.getName();
 				client.makeDirectory(newRemoteFolder);
 				for (File f : files) {
-					loadFiles(client, f, newRemoteFolder);
+					loadFiles(client, f.getAbsolutePath(), newRemoteFolder);
 				}
 			} else {
-				InputStream inputStream = new FileInputStream(file);
-				client.storeFile(remoteFolder + "\\" + file.getName(), inputStream);
+				InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+				client.storeFile(remoteFile + "\\" + file.getName(), inputStream);
 				inputStream.close();
 			}
 		}
 	}
 
+	public void downloadFiles(ClientFTP client, String localFile, String remoteFile) throws IOException {
+		FTPFile[] serverFiles = client.listFiles(remoteFile);
+		
+		for (FTPFile f : serverFiles) {
+			File downloadFile;
+			if (f.isDirectory()) {
+				
+				downloadFile = new File(localFile);
+				
+				File newFolder = new File(downloadFile, f.getName());
+				
+				newFolder.mkdir();
+				
+				//if (!downloadFile.exists()) {
+				//	downloadFile.mkdir();
+				//}
+				
+				downloadFiles(client, newFolder.getAbsolutePath(), remoteFile + "\\" + f.getName());
+			} else {
+				
+				/*File parentDir = downloadFile.getParentFile();
+				if (!parentDir.exists()) {
+					parentDir.mkdir();
+				}*/
+				downloadFile = new File(localFile, f.getName());
+				
+				OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(downloadFile));
+				try {
+					client.retrieveFile(f.getName(), outputStream);
+				} catch (IOException ex) {
+					throw ex;
+				} finally {
+					if (outputStream != null) {
+						outputStream.close();
+					}
+				}
+			}
+		}
+	}
 }
---
+++ src/general/ConfigurationData.java
@@ -0,0 +1,41 @@
+package general;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigurationData {
+	public enum Property {
+		ftpServer, ftpPort
+	}
+
+	private InputStream inputStream;
+
+	public String ReadProperty(String propertyToRead) throws IOException {
+		String result = "";
+
+		try {
+			Properties prop = new Properties();
+			String propFileName = "config.resources";
+
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("Property file '" + propFileName + "' not found in the classpath");
+			}
+
+			result = prop.getProperty(propertyToRead);
+
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+
+		return result;
+	}
+
+}
