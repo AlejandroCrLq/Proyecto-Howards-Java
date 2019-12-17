c8173ced515ce0635fbb394fcbdef9c9f98219cb
oscarbm27 <obrenesmancera.sanjose@alumnado.fundacionloyola.net>
2019-12-17 08:40:47 +0100

    Se incluye código

--- src/general/MainMenuController.java
+++ src/general/MainMenuController.java
@@ -1,5 +1,63 @@
 package general;
 
-public class MainMenuController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.mail.MessagingException;
+import javax.swing.JOptionPane;
+
+import ftp.ClientFTP;
+import ftp.FTPController;
+import ftp.Users;
+import interfaces.FTPWindow;
+import interfaces.MailWindow;
+import interfaces.StartMenu;
+
+public class MainMenuController implements ActionListener{
+	Users user;
+	StartMenu StartMenu;
+	
+	public MainMenuController(Users user) {
+		this.user = user;
+		StartMenu = new StartMenu();
+		AsignarEventos();
+		
+	}
+	
+	public void AsignarEventos() {
+		StartMenu.getBtnFTP().addActionListener(this);
+		StartMenu.getBtnMail().addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==StartMenu.getBtnFTP()) {			
+			FTPWindow FtpWindow = new FTPWindow();
+			MailWindow MailWindow;
+			try {
+				MailWindow = new MailWindow();
+				FTPController FtpController = new FTPController(MailWindow, FtpWindow, user);
+				
+				FtpController.getFtpWindow().setVisible(true);
+				FtpController.getFtpWindow().getLblDirectory().setText("/");;
+				try {
+					ClientFTP clientFTP = new ClientFTP(user);
+					clientFTP.connect();
+					if(!clientFTP.login()) {
+						JOptionPane.showMessageDialog(FtpController.getFtpWindow(), "Error al loguearse, no existe el usuario en el servidor FTP, contacte con el administrador del sistema.");
+					}else {
+						FtpController.AsignarEventos();
+					}					
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}				
+			} catch (MessagingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else {
+			
+		}
+	}
 }
